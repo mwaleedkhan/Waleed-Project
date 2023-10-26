924171cfc2d77adeb4b2f466f791eaa6f9e51597
Muhammad Waleed Khan <mwaleedkhan-developer@outlook.com>
2023-10-26 13:30:29 +0500

    Add project files.

---
+++ Waleed Project/Source.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() 
+{
+	string students; 
+	char choice;
+
+	do 
+	{
+		string currentStudent;
+		cout << "Enter Student Name: ";
+		getline(cin, currentStudent);
+		cin.ignore();
+		students.append(currentStudent);
+		students.append("\n");
+
+		cout << "Want to Continue Entering Press Y or y: ";
+		cin >> choice; 
+		cin.ignore();
+
+	} while (choice == 'Y' || choice == 'y');
+
+	cout << students; 
+	system("pause");
+	return 0;
+}
---
+++ Waleed Project/Source1.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() 
+//{
+//	int rows; 
+//	int incParent=1; 
+//
+//	cout << "Enter The Number of Rows: ";
+//	cin >> rows; 
+//
+//	//Parent While
+//	while (incParent <= rows) 
+//	{
+//		int incChild = 1;
+//		while(incChild<=incParent)
+//		{
+//			cout << incChild << " ";
+//			incChild++;
+//		}
+//		cout << endl;
+//		incParent++;
+//	}
+//
+//	system("pause");
+//	return 0; 
+//}
